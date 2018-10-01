41479f63ac6695ff0521da0a8b5d4abea20cac91
unknown <C:\Mail>
2018-10-01 17:39:19 +0300

    add method fot Cristin

--- src/main/java/tryDirect/forMe/Other.java
+++ src/main/java/tryDirect/forMe/Other.java
@@ -1,7 +1,20 @@
 package tryDirect.forMe;
 
-public class Other {
-        public static void main(String[] args) throws Exception {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-        }
+public class Other {
+    public static void main(String[] args) throws Exception {
+        Other person = new Other();
+        person.cristinNormPatsan();
+
     }
+
+    void cristinNormPatsan() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Назови свое имя ");
+       String name =  reader.readLine();
+        System.out.println(name +" Ты серьезно ?, Так себе у тебя имя ");
+    }
+}
