512079c4cb85c4ce103d2aeb70fcc75d27e21f2a
unknown <C:\Mail>
2018-10-01 17:54:32 +0300

    new changes

--- src/main/java/tryDirect/forMe/Other.java
+++ src/main/java/tryDirect/forMe/Other.java
@@ -7,14 +7,8 @@ import java.io.InputStreamReader;
 public class Other {
     public static void main(String[] args) throws Exception {
         Other person = new Other();
-        person.cristinNormPatsan();
 
     }
 
-    void cristinNormPatsan() throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Назови свое имя ");
-       String name =  reader.readLine();
-        System.out.println(name +" Ты серьезно ?, Так себе у тебя имя ");
-    }
+
 }
